84ce5848c316e1e97cba8542b841ba1986b7f479
Iker Murga <iker@code4jobs.com>
2018-10-03 21:21:04 +0200

    Add project files.

---
+++ Contadores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
